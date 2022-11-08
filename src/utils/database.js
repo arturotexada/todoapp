233cc326aef6d861081c809ef29cd560b6ff06af
Portafolio FullStack Developer de ArturoTexada <arturo.tejada@gmail.com>
2022-11-07 20:45:31 -0600

    start added

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,13 +3,22 @@
 
 const { Sequelize } = require("sequelize");
 
+// const db = new Sequelize({
+//   database: "todo",
+//   username: "postgres",
+//   host: "localhost",
+//   port: "5433",
+//   password: "root",
+//   dialect: "postgres",
+// });
+
 const db = new Sequelize({
-  database: "todo",
-  username: "postgres",
-  host: "localhost",
-  port: "5432",
-  password: "root",
-  dialect: "postgres",
+  database: process.env.DB_NAME || 'todo'
+  username: process.env.DB_USER || 'postgres'
+  host: process.env.DB_HOST || 'localhost'
+  port: process.env.DB_PORT || 5433
+  password: process.env.DB_PASSWORD || '12345678'
+  dialect: "postgres", 
 });
 
 module.exports = db;
